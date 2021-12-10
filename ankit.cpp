16074957d38f04c766a8cb43fdb8f6d2ff116c4a
Mahi Jain <Mahi Jain>
2021-12-10 16:11:26 +0530

    adding all

---
+++ ankit.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+    
+    string s1;
+    while(true){
+    cout<<" Enter your name :"<<endl;
+    cin>>s1;
+    if(s1=="jisi"||s1=="Jisi"){
+        cout<<"You are dhor"<<endl;
+    }
+    else if (s1=="Kshama"||s1=="kshama"){
+        cout<<"Aap badi ho to ..... you are Mahadhor"<<endl;
+    }
+    else if(s1=="santosh"||s1=="santosh"||s1=="jyoti"||s1=="Jyoti"){
+        cout<<"Jai Jinendra!!!"<<endl;
+    }
+        else if(s1=="Gungun"||s1=="Mahi"||s1=="gungun"||s1=="mahi"){
+        cout<<"Ore dadda re,itti soi ne sochi ti ke aapki sabse chhoti ladki sabse hoshiyar hai"<<endl;
+        }
+    
+    else if (s1=="Ankit"||s1=="ankit"){
+
+
+    cout<<"*                                                              *"<<endl;
+    cout<<"**                                                            **"<<endl;
+    cout<<"***                                                          ***"<<endl;
+    cout<<"*****                                                      *****"<<endl;
+    cout<<"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"<<endl;
+    cout<<"*****                                                      *****"<<endl;
+    cout<<"***                                                          ***"<<endl;
+    cout<<"**                                                            **"<<endl;
+    cout<<"*                                                              *"<<endl;
+    cout<<"##    ##     ########      ########     ########      ##     ##"<<endl;
+    cout<<"##    ##     ##    ##      ##    ##     ##    ##        ##   ## "<<endl;
+    cout<<"########     ########      ########     ########          ## ##"<<endl;
+    cout<<"##    ##     ##    ##      ##           ##                   ##"<<endl;
+    cout<<"##    ##     ##    ##      ##           ##                   ##"<<endl; 
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"########      ########      ########     ########         ##    ##"<<endl;
+    cout<<"##     ##        ##         ##    ##        ##            ##    ##"<<endl;
+    cout<<"########         ##         ########        ##            ########"<<endl;
+    cout<<"##     ##        ##         ## ##           ##            ##    ##"<<endl;
+    cout<<"########         ##         ##    ##        ##            ##    ##"<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"########     ########     ##    ##"<<endl;
+    cout<<"##    ##     ##    ##      ##  ##"<<endl;
+    cout<<"##    ##     ########       ####"<<endl;
+    cout<<"##    ##     ##    ##        ## "<<endl;
+    cout<<"########     ##    ##        ##  "<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"                              #########"<<endl;
+    cout<<"                             ##        ##  "<<endl;
+    cout<<"           @                 ##        ##  "<<endl;
+    cout<<"*********************************************************************"<<endl;
+    cout<<"######     ##       ##       ##        ##        ##        ##  ##"<<endl;
+    cout<<"   ##      ##       ##       ##        ##    ##    ##      ##  ##"<<endl;
+    cout<<"  ###########   ##########   ##   #######           ##     ##  ## "<<endl;
+    cout<<"    ##     ##   ##  ##  ##   ##   #######          ##     ###  ##"<<endl;
+    cout<<"  ##       ##   ###### ###   ##   ##   ##         ##     ####  ##"<<endl;
+    cout<<"           ##       ##       ##   ##   ##           ####   ##  ##  "<<endl;
+    cout<<"                    #####"<<endl;
+    cout<<"                ##     ##"<<endl;
+    cout<<"                 ######      "<<endl;
+    break;
+    }
+
+    
+    
+    
+    else{
+        cout<<" Enter another name "<<endl;
+    }
+    }
+    return 0;
+}
